07cc9fe5285bd739d0dbde15b3e5a1d4d40ae586
Sensei203 <sensei203sensei@gmail.com>
2022-08-21 22:41:57 +0300

    onclite: prebuilt: Add GoogleCameraGo apps

--- device.mk
+++ device.mk
@@ -282,10 +282,13 @@ PRODUCT_COPY_FILES += \
 
 # Netutils
 PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0 \
     android.system.net.netd@1.0 \
-    android.system.net.netd@1.1.vendor \
-    libandroid_net \
-    netutils-wrapper-1.0
+    libandroid_net
+	
+# GoogleCameraGo
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
 
 # OMX
 PRODUCT_PACKAGES += \
