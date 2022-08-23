263905beae144f57c0e2cc7ac821fd5004145e18
Sensei205 <maxim55miui11@gmail.com>
2022-08-24 01:46:45 +0300

    onclite: prebuilt: Add ViaBrowser apps

--- device.mk
+++ device.mk
@@ -278,7 +278,11 @@ PRODUCT_PACKAGES += \
 	
 # GalleryGo
 PRODUCT_PACKAGES += \
-    GalleryGo	
+    GalleryGo
+	
+# ViaBrowser
+PRODUCT_PACKAGES += \
+    ViaBrowser
 
 # OMX
 PRODUCT_PACKAGES += \
